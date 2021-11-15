7502e6d17e6e76909a74b5ab32fc013e48ca5646
Alisher <Alisher>
2021-11-15 21:54:38 +0600

    Initial Commit

---
+++ Week10/Week10App.swift
@@ -0,0 +1,17 @@
+//
+//  Week10App.swift
+//  Week10
+//
+//  Created by Алишер Алсейт on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct Week10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
